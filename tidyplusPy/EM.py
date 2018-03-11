5fcc13c0ce76e52e23caa59c86bf3272fca41953
Akshi Chaudhary <chaudhary.akshi@gmail.com>
2018-03-11 05:50:53 -0700

    Upload EM and MMM impute function code

--- tidyplusPy/EM.py
+++ tidyplusPy/EM.py
@@ -1,58 +1,60 @@
-""" tidyplusPy.imputations.em"""
-
-import random
 import numpy as np
+import pytest
+import pandas as pd
 
+def em_run(data, loops=50):
+    """ Imputes data using expectation maximization.
+    E-step: Calculates the expected value of missing data  based on log likelihood ratio.
+    M-step: Finds the parameters that maximize the log likelihood function
 
-@checks
-def em(data, loops=50, dtype="cont"):
-    """ Imputes given data using expectation maximization.
-
-    E-step: Calculates the expected complete data log likelihood ratio.
-    M-step: Finds the parameters that maximize the log likelihood of the
-    complete data.
-
-    Parameters
-    ----------
-    data: numpy.nd.array
-        Data to impute.
-    loops: int
-        Number of em iterations to run before breaking.
-    dtype: ("cont","disc")
-        Indicates whether the possible values will come from a continuous
-        range or categorical range.
-
-    Returns
-    -------
-    numpy.nd.array
-        Imputed data.
-        
-    Reference: Elton Law
-        https://github.com/eltonlaw/impyute
-
+    @param
+      data: numpy.nd.array : Data to impute.
+      loops: int : Number of em iterations to run before breaking.
+      Returns : numpy.nd.array, Imputed data.
+      
+    Attribution : https://github.com/eltonlaw/impyute
     """
-    if dtype == "cont":
-        null_xy = find_null(data)
-        for x_i, y_i in null_xy:
-            col = data[:, int(y_i)]
+    null_xy = np.argwhere(np.isnan(data)) ## check indices of all missing value
+    for x_i, y_i in null_xy:
+        col = data[:, int(y_i)]
+        mu = col[~np.isnan(col)].mean() ## get col means
+        std = col[~np.isnan(col)].std() ## get col standard deviation
+        col[x_i] = random.gauss(mu, std) ## create gaussian distribution of mu and std
+        previous, i = 1, 1
+        for i in range(loops):
+            # Expectation
             mu = col[~np.isnan(col)].mean()
             std = col[~np.isnan(col)].std()
-            col[x_i] = random.gauss(mu, std)
-            previous, i = 1, 1
-            for i in range(loops):
-                # Expectation
-                mu = col[~np.isnan(col)].mean()
-                std = col[~np.isnan(col)].std()
-                # Maximization
-                col[x_i] = random.gauss(mu, std)
-                # Break out of loop if likelihood doesn't change at least 10%
-                # and has run at least 5 times
-                delta = (col[x_i]-previous)/previous
-                if i > 5 and delta < 0.1:
-                    data[x_i][y_i] = col[x_i]
-                    break
+            # Maximization
+            col[x_i] = random.gauss(mu, std) #update parameters to maximize
+            delta = (col[x_i]-previous)/previous  ## record change in parameters
+            if i > 5 and delta < 0.1:
                 data[x_i][y_i] = col[x_i]
-                previous = col[x_i]
+                break  ## break when likelihood doesn't change at least 10%
+            data[x_i][y_i] = col[x_i]
+            previous = col[x_i]
         return data
     else:
         raise Exception("Other dtypes not supported yet.")
+
+def em(data,loops=20):
+    while len(np.argwhere(np.isnan(data))) > 0: ## run this step to update all columns in the matrix
+        em_run(data)
+    return data
+
+"""Dummy Dataset"""
+
+# matrix= np.random.rand(5,4)
+# matrix[1,0] = np.nan
+# matrix[2,1] = np.nan
+# matrix[4,2] = np.nan
+# matrix[3,3] = np.nan
+# matrix
+
+"""Usage"""
+
+# em(matrix,loops = 20)
+    
+    
+
+
--- tidyplusPy/mmm.py
+++ tidyplusPy/mmm.py
@@ -1,43 +1,66 @@
-""" tidyplusPy.imputations.mmm"""
+""" tidyplusPy.mmm"""
 
 import random
 import numpy as np
+import pandas as pd
+import six
 
 
-
-def mmm(data, method):
-    """Multivariate Imputation by Mode
+def mmm(data, method= "mean/median/mode"):
+    """Multivariate Imputation by Mean, median or mode
     ----------
-    data: numpy.ndarray
+    data: Pandas data frame
         Data to impute.
     method : could be Mean,Median,Mode
     RETURNS
     -------
-    numpy.ndarray
-        Imputed data.
+        Imputed data: Pandas dataframe
     """
-    null_xy = find_null(data)
+    df = dataframe
+    df_num = df._get_numeric_data() ### Get only numeric columns from dataframe
+    if method == "mean" :
+        out = df_num.apply(lambda x: x.fillna(x.mean()),axis=0)
+        a = []
+        for i in df.columns.values.tolist() :
+            if i not in out.columns.values.tolist() :
+                a.append(i)     ## Step to identify non-numeric columns
 
-    # Add a column of zeros to the index values
-    null_xyv = np.append(null_xy, np.zeros((np.shape(null_xy)[0], 1)), axis=1)
+        result = pd.concat([out, df.ix[:,a]], axis=1) # merge all columns for result
+    
+    
+    elif method == "median" :
+        out = df_num.apply(lambda x: x.fillna(x.median()),axis=0) ### Get only numeric columns from dataframe
+        a = []
+        for i in df.columns.values.tolist() :
+            if i not in out.columns.values.tolist() :
+                a.append(i)  ## Step to identify non-numeric columns
 
-    null_xyv = [[int(x), int(y), v] for x, y, v in null_xyv]
-    temp = []
-    cols_missing = set([y for _, y, _ in null_xyv])
+        result = pd.concat([out, df.ix[:,a]], axis=1) # merge all columns for result
+    
+    
+    elif method == "mode" :
+        result = df.apply(lambda x: x.fillna(x.mode()[0]),axis=0) ## this is applied to both numeric and character values
+    else :
+        print("missing can't be imputed")
+    
+    return result ## imputed dataset
 
 
-        # Step 2: Missing values for the missing variable/column are replaced
-        # with predictions from our new linear regression model
-        temp = []
-        # For null indices with the dependent column that was randomly chosen
-        for i, x_i, y_i, value in enumerate(null_xyv):
-            if y_i == dependent_col:
-                # Row 'x' without the nan value
-                new_value = model.predict(np.delete(data[x_i], dependent_col))
-                data[x_i][y_i] = new_value.reshape(1, -1)
-                temp.append([x_i, y_i, new_value])
-                delta = (new_value-value)/value
-                if delta < 0.1:
-                    converged[i] = True
-        null_xyv = temp
-    return data
+
+
+"""Dummy Dataset"""
+
+# NaN = float('nan')
+# ID = [1, 2, 3, 4, 5, 6, 7]
+# A = [NaN, NaN, NaN, 0.1, 0.3, 0.3, 0.4]
+# B = [0.2, NaN, 0.2, 0.7, 0.9, NaN, NaN]
+# C = [NaN, 'A', 'B', NaN, 'C', 'D', 'D']
+# D = [NaN, 'C', 'E', NaN, 'C', 'H', 'D']
+# columns = {'A':A, 'B':B, 'C':C, 'D':D}
+# df = pd.DataFrame(columns, index=ID)
+# df.index.name = 'ID'
+# print(df)
+
+"""Usage"""
+
+# mmm(df,method = "mode")
