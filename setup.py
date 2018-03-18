6888b0cee846a65f8f3d0c7713c13217f2399862
Xinbin Huang <xbhuang93@hotmail.com>
2018-03-18 12:02:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='tidyplusPy',
     author='Akshi Chaudhary, Yue (Tina) Qian, Xinbin Huang',
     version='v4.0',
-    url = 'https://github.com/UBC-MDS/tidyplusPy',
     packages=['tidyplusPy',],
+    url = 'https://github.com/UBC-MDS/tidyplusPy',
     license='MIT',
     description = 'Extra tools for data wrangling',
     long_description=open('README.txt').read(),
+    
     install_requires = ['numpy','pandas'],
+    include_package_data=True,
 )
